7da782ca81b81fa37bf5a5bd4bc049f64b7c3def
Mikael Vandmo <mikael@vandmo.se>
2019-02-07 11:05:30 +0100

    Try fix deploy

--- deployment/deploy.bash
+++ deployment/deploy.bash
@@ -1,7 +1,6 @@
 set -euxo pipefail
 
 mvn versions:set \
-  --file maven-plugin/pom.xml \
   --batch-mode \
   --define newVersion=${TRAVIS_TAG}
 
@@ -10,12 +9,11 @@ gpg2 --keyring=${TRAVIS_BUILD_DIR}/pubring.gpg --no-default-keyring --import dep
 gpg2 --allow-secret-key-import --keyring=${TRAVIS_BUILD_DIR}/secring.gpg --no-default-keyring --import deployment/signingkey.asc || true
 
 mvn clean deploy \
-  --file maven-plugin/pom.xml \
   --batch-mode \
   --settings deployment/settings.xml \
   --profile gpg.sign
   --define gpg.executable=gpg2 \
-  --define gpg.keyname=B07746099C297F239C1C4343E16B0657F1774582 \
+  --define gpg.keyname=9368046AC6F2656D2D9FD382EDC4AF47B8A88C15 \
   --define gpg.passphrase=${PASSPHRASE} \
   --define gpg.publicKeyring=${TRAVIS_BUILD_DIR}/pubring.gpg \
   --define gpg.secretKeyring=${TRAVIS_BUILD_DIR}/secring.gpg \
