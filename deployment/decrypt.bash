7958f3a38175aae281b01bc12a3bcc4ef564f106
Mikael Vandmo <mikael@vandmo.com>
2019-02-07 10:41:48 +0100

    Try fix signing

--- deployment/decrypt.bash
+++ deployment/decrypt.bash
@@ -1,6 +1,3 @@
 set -euxo pipefail
 
-openssl aes-256-cbc \
-  -k "${PASSPHRASE}" \
-  -in deployment/signingkey.asc.enc \
-  -out deployment/signingkey.asc -d
+openssl aes-256-cbc -K $encrypted_9ef43f452f42_key -iv $encrypted_9ef43f452f42_iv -in deployment/signingkey.asc.enc -out deployment/signingkey.asc -d
