6f3a8dd3418e04d56bb8c15028571cdc90342bf4
Mikael Vandmo <mikael@vandmo.se>
2022-11-03 19:23:00 +0100

    Group tests in folders

--- src/it/pom/lock/postbuild.groovy
+++ src/it/pom/lock/postbuild.groovy
@@ -8,11 +8,11 @@ import static org.junit.Assert.assertTrue
 
 import org.apache.commons.io.FileUtils
 
-lockFile = new File(basedir, "dependencies-lock.json")
-expectedLockFile = new File(basedir, "expected-dependencies-lock.json")
+lockFile = new File(basedir, ".dependency-lock/pom.xml")
+expectedLockFile = new File(basedir, "expected-pom.xml")
 
-assertTrue("Lock file missing", lockFile.isFile())
-assertTrue("Lock file content not as expected", FileUtils.contentEquals(expectedLockFile, lockFile))
+assertTrue(lockFile.isFile())
+assertTrue(FileUtils.contentEquals(expectedLockFile, lockFile))
 
 buildLog = FileUtils.readLines(new File(basedir, "build.log"), UTF_8)
-assertThat(buildLog, hasItem(both(startsWith("[INFO] Creating ")).and(endsWith("/dependency-lock-maven-plugin/target/its/json-lock/dependencies-lock.json"))))
+assertThat(buildLog, hasItem(both(startsWith("[INFO] Creating ")).and(endsWith("/dependency-lock-maven-plugin/target/its/pom/lock/.dependency-lock/pom.xml"))))
