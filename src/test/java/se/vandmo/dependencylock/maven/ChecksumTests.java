574478eae90bc12fd882a7a486bb345f180d47ca
Smaarn <antoine.malliarakis@gmail.com>
2025-04-23 15:11:46 +0200

    Add test on optimized code

--- src/test/java/se/vandmo/dependencylock/maven/ChecksumTests.java
+++ src/test/java/se/vandmo/dependencylock/maven/ChecksumTests.java
@@ -4,9 +4,15 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 public final class ChecksumTests {
+
+  @Rule public final TemporaryFolder folder = new TemporaryFolder();
 
   @Test(expected = NullPointerException.class)
   public void calculateFor_byteArrayNull() {
@@ -23,5 +29,14 @@ public final class ChecksumTests {
     assertEquals(
         "sha512:23I/NBoELY3hqoE+/V4C/BdFzL4llIYldRSATi7EvOuypG8eStRCFUlD+el+G8R8OuDt2rfeDAGpxR8VNCpbGQ==",
         Checksum.calculateFor("abcdefghijklmno".getBytes(UTF_8)));
+  }
+
+  @Test
+  public void calculateForFile() throws IOException {
+    final File file = folder.newFile("abcdefghijklmno.txt");
+    Files.write(file.toPath(), "abcdefghijklmno".getBytes(UTF_8));
+    assertEquals(
+        "sha512:23I/NBoELY3hqoE+/V4C/BdFzL4llIYldRSATi7EvOuypG8eStRCFUlD+el+G8R8OuDt2rfeDAGpxR8VNCpbGQ==",
+        Checksum.calculateFor(file));
   }
 }
