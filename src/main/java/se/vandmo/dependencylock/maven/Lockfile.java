6b8db08534888bffb825f4619582921b68dbb57d
Mikael Vandmo <mikael@vandmo.se>
2025-04-26 17:13:03 +0000

    Explain why version in lock file could mismatch

--- src/main/java/se/vandmo/dependencylock/maven/Lockfile.java
+++ src/main/java/se/vandmo/dependencylock/maven/Lockfile.java
@@ -1,8 +1,10 @@
 package se.vandmo.dependencylock.maven;
 
+import org.apache.maven.plugin.MojoExecutionException;
+
 public interface Lockfile {
 
   void write(LockedProject contents);
 
-  LockedProject read();
+  LockedProject read() throws MojoExecutionException;
 }
