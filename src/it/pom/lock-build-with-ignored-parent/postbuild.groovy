79aaed85f1edf313f0a24d841af0128d8cc7024d
Smaarn <antoine.malliarakis@gmail.com>
2026-03-23 14:19:10 +0100

    fix: add test to validate ignoring of signature and version is also enforced on parent chain

---
+++ src/it/pom/lock-build-with-ignored-parent/postbuild.groovy
@@ -0,0 +1,32 @@
+import static java.nio.charset.StandardCharsets.UTF_8
+import static org.hamcrest.MatcherAssert.assertThat
+import static org.hamcrest.core.CombinableMatcher.both
+import static org.hamcrest.core.IsIterableContaining.hasItem
+import static org.hamcrest.core.StringEndsWith.endsWith
+import static org.hamcrest.core.StringStartsWith.startsWith
+import static org.junit.Assert.assertTrue
+
+import org.apache.commons.io.FileUtils
+
+referenceDirectory = basedir
+mavenSpecificReferenceDirectory = new File(referenceDirectory, mavenVersion)
+if (mavenSpecificReferenceDirectory.exists()) {
+    referenceDirectory = mavenSpecificReferenceDirectory
+    System.out.println("Using maven specific reference directory: ${referenceDirectory}")
+}
+
+def assertLockFile(lockFilename, expectedFilename) {
+    lockFile = new File(basedir, lockFilename)
+    expectedLockFile = new File(referenceDirectory, expectedFilename)
+    assertTrue(lockFile.isFile())
+    assertTrue(expectedLockFile.isFile())
+    assertTrue(FileUtils.contentEquals(expectedLockFile, lockFile))
+}
+
+assertLockFile(".dependency-lock/pom.xml", "expected/pom.xml")
+assertLockFile(".dependency-lock/parents/pom.xml", "expected/parents/pom.xml")
+assertLockFile(".dependency-lock/plugins/pom.xml", "expected/plugins/pom.xml")
+assertLockFile(".dependency-lock/extensions/pom.xml", "expected/extensions/pom.xml")
+
+buildLog = FileUtils.readLines(new File(basedir, "build.log"), UTF_8)
+assertThat(buildLog, hasItem(both(startsWith("[INFO] Creating ")).and(endsWith("/dependency-lock-maven-plugin/target/its/pom/lock-build-with-ignored-parent/.dependency-lock/pom.xml"))))
